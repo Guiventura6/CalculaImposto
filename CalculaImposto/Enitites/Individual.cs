5c7bfd59953b07fe05b6a6331cbec75c807d2ae4
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-29 16:56:23 -0300

    Criacaçao da classe Individual e validacao de instaciacao no arquivo program

---
+++ CalculaImposto/Enitites/Individual.cs
@@ -0,0 +1,28 @@
+﻿namespace CalculaImposto.Enitites
+{
+    class Individual : TaxPayers
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double TaxesPaid()
+        {
+            double tax, healthDisc = 0;
+            if (AnualIncome < 20000.00)
+                tax = AnualIncome * 15.0 / 100.0;
+            else
+                tax = AnualIncome * 25.0 / 100.0;
+
+            if (HealthExpenditures > 0)
+            {
+                healthDisc = HealthExpenditures * 50.0 / 100.0;
+            }
+
+            return tax - healthDisc;
+        }
+    }
+}
--- CalculaImposto/Program.cs
+++ CalculaImposto/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using CalculaImposto.Enitites;
 
 namespace CalculaImposto
 {
@@ -6,7 +8,15 @@ namespace CalculaImposto
     {
         static void Main(string[] args)
         {
-            
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            TaxPayers taxPayers = new Individual("Joao", 50000.00, 2000.00);
+
+            Console.WriteLine(taxPayers.Name); 
+            Console.WriteLine(taxPayers.AnualIncome); 
+            Console.WriteLine(taxPayers.TaxesPaid().ToString("F2", CI)); 
+
+
         }
     }
 }
