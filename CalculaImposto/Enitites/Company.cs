b15f37c549bb39158e265031e705e66f1052187c
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-29 17:20:11 -0300

    Criacao da classe Company

---
+++ CalculaImposto/Enitites/Company.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculaImposto.Enitites
+{
+    class Company : TaxPayers
+    {
+
+        public int NumberOfEmployee { get; set; }
+
+        public Company(string name, double anualIncome, int numberOfEmployee) : base(name, anualIncome)
+        {
+            NumberOfEmployee = numberOfEmployee;
+        }
+
+        public override double TaxesPaid()
+        {
+            if (NumberOfEmployee > 10)
+                return AnualIncome * 14.0 / 100.0;
+            else
+                return AnualIncome * 16.0 / 100.0;
+        }
+    }
+}
