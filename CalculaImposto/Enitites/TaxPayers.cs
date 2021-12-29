9a20e1856fa054078af30af53f7a6198de579899
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2021-12-29 16:24:35 -0300

    Criacao da classe TaxPayers

---
+++ CalculaImposto/Enitites/TaxPayers.cs
@@ -0,0 +1,16 @@
+﻿namespace CalculaImposto.Enitites
+{
+    abstract class TaxPayers
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        public TaxPayers(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double TaxesPaid();
+    }
+}
--- CalculaImposto/Program.cs
+++ CalculaImposto/Program.cs
@@ -6,7 +6,7 @@ namespace CalculaImposto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
